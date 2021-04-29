3aeee34a5982d8a981fe6e5f080ac5aad66a8feb
quanghiepth86 <quanghiepth86@gmail.com>
2021-04-29 16:09:49 +0700

    Remove unsed code

--- Modules/Exercise02/Services/ATMService.php
+++ Modules/Exercise02/Services/ATMService.php
@@ -13,34 +13,6 @@ class ATMService
     const TIME_PERIOD_3 = ['18:00', '23:59'];
     const HOLIDAYS = ['01-01', '30-04', '01-05'];
 
-
-     /**
-     * Calcuate ATM Fee for vip customer
-     *
-     *
-     * @return null
-     */
-    public function calculateFeeForVipCustomer()
-    {
-        if ($isVip) {
-            return self::NO_FEE;
-        }
-
-        // Return normalFee if it is holiday or weekend
-        $transactionDate = Carbon::parse($transactionAt);
-        if (in_array($transactionDate->format('d-m'), self::HOLIDAYS) || $transactionDate->isWeekend()) {
-            return self::NORMAL_FEE;
-        }
-
-        $timeNow = $transactionDate->format('H:i');
-        list($minTimePeriod2, $maxTimePeriod2) = self::TIME_PERIOD_2;
-        if ($timeNow >= $minTimePeriod2 && $timeNow <= $maxTimePeriod2) {
-            return self::NO_FEE;
-        }
-
-        return self::NORMAL_FEE;
-    }
-
     /**
      * Calcuate ATM Fee
      *

b6018ab074675864de781722438b55fcaf63e5a9
tran.cong.tu@framgia.com <tU210819945>
2021-05-04 18:51:38 +0700

    trancongtu-exercisers-unitest

---
+++ Modules/Exercise03/Tests/Unit/Repositories/ProductRepositoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Exercise03\Tests\Unit\Http\Requests;
+
+use Tests\TestCase;
+use Modules\Exercise03\Repositories\ProductRepository;
+use Mockery as m;
+use Modules\Exercise03\Models\Product;
+
+class ProductRepositoryTest extends TestCase
+{
+    protected $productRepository;
+    protected $model;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->model = m::mock(Product::class)->makePartial();
+        $this->productRepository = new ProductRepository($this->model);
+    }
+
+    public function test_find()
+    {
+        $this->model->shouldReceive('all')->andreturn([]);
+
+        $result = $this->productRepository->all(1);
+        $this->assertEquals([], $result);
+    }
+}
